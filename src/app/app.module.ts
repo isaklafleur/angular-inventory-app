17819f34522880e6aa200a9633a5e7789d8e15d6
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-06 16:25:46 +0100

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ProductsListComponent } from "./components/products-list/products-list.component";
+import { ProductRowComponent } from './components/product-row/product-row.component';
+import { ProductImageComponent } from './components/product-image/product-image.component';
+import { PriceDisplayComponent } from './components/price-display/price-display.component';
+import { ProductDepartmentComponent } from './components/product-department/product-department.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductsListComponent, ProductRowComponent, ProductImageComponent, PriceDisplayComponent, ProductDepartmentComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
